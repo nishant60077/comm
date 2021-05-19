8b7ddaa29e8e2e8996f16321a77d6dbed9ab7864
Nishant Pandey <nishant60077@gmail.com>
2021-05-19 19:44:31 +0530

    final commit

--- commission_api/settings.py
+++ commission_api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '3vkxubevd*db=i!k$#^-rhm!-5vzjje-z_n8aaxa!s5qw+(+pc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [127.0.0.1]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- commission_api/urls.py
+++ commission_api/urls.py
@@ -13,10 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.views.static import serve
+from django.conf.urls import url
+from django.conf.urls.static import ststic
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    url(r'^media/(?p<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^media/(?p<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
 ]
+
+
